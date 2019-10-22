bb5570a1c484de582431f414c8ea8112cee9d6fd
XDyao <56805700+XDyao@users.noreply.github.com>
2019-10-21 23:10:43 -0300

    Update cadastro.c

--- src/cadastro.c
+++ src/cadastro.c
@@ -1,9 +1,10 @@
 #include "cadastro.h"
 
-void set_cadastro(cadastro *carro, int plate, int in, int hours, int out){
+void set_cadastro(cadastro *carro, int plate, int in, int hours, int out, int boolean){
     cadastro.placa = plate;
     cadastro.entrada = in;
     cadastro.n_horas = hours;
     cadastro.saida = out;
+    cadastro.desconto = boolean;
     return;
 }
